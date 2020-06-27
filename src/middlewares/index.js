b36fd56ad0293edcc40bbe3d6b64a98365dedf6f
José Luis <joseluismejiarojas@gmail.com>
2020-06-27 09:54:15 -0500

    [ADD] caching implement

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,5 +3,6 @@ module.exports = {
     ErrorMiddleware: require('./error.middleware'),
     AuthMiddleware: require('./auth.middleware'),
     ParseIntMiddleware: require('./parse-int.middleware'),
+    CacheMiddleware: require('./cache.middleware'),
 
 }
