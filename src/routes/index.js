8dee269be15cc7eb01af73333e84d9d392259f45
José Luis <joseluismejiarojas@gmail.com>
2020-06-27 10:39:35 -0500

    [ADD] swagger implemented

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+require('express-async-errors');
 const { NotFoundMiddleware, ErrorMiddleware } = require('../middlewares');
 
-require('express-async-errors');
+const swaggerUI = require('swagger-ui-express');
+const { SWAGGER_PATH } = require('../config')
+const swaggerDocument = require(SWAGGER_PATH);
 
 module.exports = function({
     HomeRoutes,
@@ -29,6 +32,7 @@ module.exports = function({
 
 
     router.use('/v1/api', apiRoutes);
+    router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
     router.use(NotFoundMiddleware);
     router.use(ErrorMiddleware);
 
